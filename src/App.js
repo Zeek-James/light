91b23f25b8db726647e01ce29a4099a6873fc1e7
Zeek James <65875140+Zeek-James@users.noreply.github.com>
2022-02-03 23:46:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-// import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-// import { NWC } from "./assets/utils";
-// import { Payment } from "./components/payment-info/Payment";
-// import { getDetails } from "./store/action";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
+import { NWC } from "./assets/utils";
+import { Payment } from "./components/payment-info/Payment";
+import { getDetails } from "./store/action";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function App() {
-//   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     dispatch(getDetails());
-//   }, [dispatch]);
+  useEffect(() => {
+    dispatch(getDetails());
+  }, [dispatch]);
 
-//   const getTotal = localStorage.getItem("total");
+  const getTotal = localStorage.getItem("total");
 
-//   const total = JSON.parse(getTotal);
+  const total = JSON.parse(getTotal);
 
   return (
     <div className="App">
       <Navbar />
 
-//       <Container>
-//         <Payment />
-//         <Box
-//           sx={{
-//             display: "flex",
-//             alignItems: "center",
-//             justifyContent: "space-between",
-//             marginBottom: 8,
-//           }}
-//         >
-//           <Button variant="contained" size="large">
-//             Complete payment
-//           </Button>
-//           <Typography sx={{ fontWeight: 900 }}>
-//             Total: <span>N{NWC(total)}</span>
-//           </Typography>
-//         </Box>
-//       </Container>
+      <Container>
+        <Payment />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: 8,
+          }}
+        >
+          <Button variant="contained" size="large">
+            Complete payment
+          </Button>
+          <Typography sx={{ fontWeight: 900 }}>
+            Total: <span>N{NWC(total)}</span>
+          </Typography>
+        </Box>
+      </Container>
     </div>
   );
 }
